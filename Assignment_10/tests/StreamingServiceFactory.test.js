ba846a0a539cba3a52af51479c9450df88ff789a
Daniel Marais <139280004+GrandDadDan@users.noreply.github.com>
2025-04-20 19:42:16 +0200

    Update StreamingServiceFactory.test.js

--- Assignment_10/tests/StreamingServiceFactory.test.js
+++ Assignment_10/tests/StreamingServiceFactory.test.js
@@ -1,1 +1,7 @@
+import StreamingServiceFactory from '../creational_patterns/abstract_factory/StreamingServiceFactory.js';
+
+test('should return Netflix streaming object', () => {
+  const platform = StreamingServiceFactory.getStreamingPlatform('netflix');
+  expect(platform.stream()).toBe('Streaming from Netflix');
+});
 

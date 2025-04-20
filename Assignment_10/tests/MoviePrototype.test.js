fdc357367cd41108a02fd19eb1f6891fd9a87dcd
Daniel Marais <139280004+GrandDadDan@users.noreply.github.com>
2025-04-20 20:21:47 +0200

    Update MoviePrototype.test.js

--- Assignment_10/tests/MoviePrototype.test.js
+++ Assignment_10/tests/MoviePrototype.test.js
@@ -1,1 +1,10 @@
+import MoviePrototype from '../creational_patterns/prototype/MoviePrototype.js';
+
+test('should clone movie with same attributes', () => {
+  const original = new MoviePrototype('Interstellar', 'Sci-Fi', 9);
+  const clone = original.clone();
+  expect(clone.title).toBe('Interstellar');
+  expect(clone.genre).toBe('Sci-Fi');
+  expect(clone.rating).toBe(9);
+});
 

20295f91bec6f4a48e3ca7754b9626b9c9187344
Daniel Marais <139280004+GrandDadDan@users.noreply.github.com>
2025-04-20 19:41:55 +0200

    Update AuthFactoryMethod.test.js

--- Assignment_10/tests/AuthFactoryMethod.test.js
+++ Assignment_10/tests/AuthFactoryMethod.test.js
@@ -1,1 +1,11 @@
+import AuthFactory from '../creational_patterns/factory_method/AuthFactoryMethod.js';
+
+test('should return Google auth handler', () => {
+  const handler = AuthFactory.getAuthHandler('google');
+  expect(handler.authenticate()).toBe('Authenticating via Google');
+});
+
+test('should throw error on invalid handler type', () => {
+  expect(() => AuthFactory.getAuthHandler('twitter')).toThrow();
+});
 

9d608e9cacc9b05b7361afc170599903558ee673
Daniel Marais <139280004+GrandDadDan@users.noreply.github.com>
2025-04-20 19:42:37 +0200

    Update WatchlistBuilder.test.js

--- Assignment_10/tests/WatchlistBuilder.test.js
+++ Assignment_10/tests/WatchlistBuilder.test.js
@@ -1,1 +1,19 @@
+import WatchlistBuilder from '../creational_patterns/builder/WatchlistBuilder.js';
+
+test('should build a watchlist with name and movies', () => {
+  const watchlist = new WatchlistBuilder()
+    .setName('My List')
+    .addMovie('Inception')
+    .setShared(true)
+    .build();
+
+  expect(watchlist.name).toBe('My List');
+  expect(watchlist.isShared).toBe(true);
+  expect(watchlist.movies).toContain('Inception');
+});
+
+test('should return empty list by default', () => {
+  const watchlist = new WatchlistBuilder().build();
+  expect(watchlist.movies.length).toBe(0);
+});
 

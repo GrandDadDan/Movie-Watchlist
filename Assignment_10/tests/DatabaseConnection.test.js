2c643aa7eb7930209ee968316301cb6a022d4ca9
Daniel Marais <139280004+GrandDadDan@users.noreply.github.com>
2025-04-20 19:43:13 +0200

    Update DatabaseConnection.test.js

--- Assignment_10/tests/DatabaseConnection.test.js
+++ Assignment_10/tests/DatabaseConnection.test.js
@@ -1,1 +1,8 @@
 
+import db1 from '../creational_patterns/singleton/DatabaseConnection.js';
+import db2 from '../creational_patterns/singleton/DatabaseConnection.js';
+
+test('should return the same instance', () => {
+  expect(db1).toBe(db2);
+  expect(db1.query('SELECT 1')).toMatch(/Executing:/);
+});
